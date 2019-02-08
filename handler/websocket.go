8dc57236d423238775f05922ce0b62c4881bab47
Praveen <selvaprvn@gmail.com>
2019-02-08 12:21:52 +0530

    init

---
+++ handler/websocket.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"context"
+	"graphql"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// MiddlewareFunc middleware
+type MiddlewareFunc func(ctx context.Context) context.Context
+
+// WebSocket handle websocket connection
+func WebSocket(schema *graphql.Schema, makeCtxFunc MiddlewareFunc) http.Handler {
+	upgrader := &websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		socket, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Printf("upgrader.Upgrade: %v", err)
+			return
+		}
+		defer socket.Close()
+
+		makeCtx := func(ctx context.Context) context.Context {
+			return makeCtxFunc(ctx)
+		}
+
+		log.Println(makeCtx(r.Context()))
+
+		//graphql.ServeJSONSocket(r.Context(), socket, schema, makeCtx, &simpleLogger{})
+	})
+}
